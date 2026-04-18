63afb9b6c65a5b3de474831f2af531c55c29791f
Chauhan Shrey Piyush <23ce115@svitvasad.ac.in>
2026-04-19 01:31:41 +0530

    Update ai.js

--- api/ai.js
+++ api/ai.js
@@ -39,7 +39,7 @@ export default async function handler(req, res) {
           "X-Title": "BlueLedger",
         },
         body: JSON.stringify({
-          model: "google/gemma-3-4b-it:free",          
+          model: "deepseek/deepseek-r1-distill-llama-70b:free",
           messages: formattedMessages,
           max_tokens: 1000,
         }),
